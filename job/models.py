43edea948f61973da8eb58739cde35eedac613e8
no3a <minomoneim@gmail.com>
2020-06-27 00:49:05 +0200

    slug

--- job/models.py
+++ job/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-
+from django.utils.text import slugify
 # Create your models here.
 
 JOP_TYPE = (
@@ -25,6 +25,12 @@ class Job(models.Model):
     categories   = models.ForeignKey('Category' , on_delete=models.CASCADE )
     image        = models.ImageField(upload_to=image_uplaod)
 
+    slug         = models.SlugField(blank=True, null=True)
+
+
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.title)
+        super(Job,self).save(*args, **kwargs)
 
     def __str__(self):
         return self.title
@@ -34,6 +40,20 @@ class Category(models.Model):
 
     name = models.CharField(max_length=25)
 
+
+    def __str__(self):
+        return self.name
+
+
+class Apply(models.Model):
+
+    job          = models.ForeignKey(Job, related_name='apply_job', on_delete=models.CASCADE)
+    name         = models.CharField(max_length=50)
+    email        = models.EmailField()
+    website      = models.URLField(blank=True, null=True)
+    cv           = models.FileField(upload_to='apply')
+    cover_letter = models.TextField(max_length=200)
+    created_at   = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return self.name
--- job/views.py
+++ job/views.py
@@ -3,7 +3,6 @@ from .models import Job
 from django.core.paginator import Paginator
 # Create your views here.
 
-
 def job_list(request):
     job_list = Job.objects.all()
 
@@ -15,7 +14,7 @@ def job_list(request):
     return render(request, 'job/job_list.html', context)
 
 
-def job_detail(request , id):
-    job_detail = Job.objects.get(id=id)
+def job_detail(request , slug):
+    job_detail = Job.objects.get(slug=slug)
     context = {'job' : job_detail}
     return render(request, 'job/job_detail.html', context)
