18da09daa87357bdb981b6036f2d07f1819f24d6
no3a <minomoneim@gmail.com>
2020-06-30 18:08:51 +0200

    edit profile

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,7 +1,7 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
-
+from .models import Profile
 
 
 class SignupForm(UserCreationForm):
@@ -9,3 +9,15 @@ class SignupForm(UserCreationForm):
 
         model  = User
         fields = ['username','email','password1','password2']
+
+
+class UserForm(forms.ModelForm):
+    class Meta:
+        model = User
+        fields = ['first_name','last_name','email']
+
+
+class ProfileForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        fields = ['phone_number','city','image']
--- accounts/views.py
+++ accounts/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render , redirect
-from .forms import SignupForm
+from .forms import SignupForm , UserForm , ProfileForm
 from django.contrib.auth import authenticate , login
 from .models import Profile
+from django.urls import reverse
 # Create your views here.
 
 
@@ -33,5 +34,20 @@ def profile(request):
 
 def profile_edit(request):
 
+    profile = Profile.objects.get(user=request.user)
 
-    return render(request,'accounts/profile_edit.html',{})
+    if request.method=='POST':
+        userform = UserForm(request.POST,instance=request.user)
+        profileform = ProfileForm(request.POST,request.FILES,instance=profile)
+        if userform.is_valid() and profileform.is_valid():
+            userform.save()
+            myprofile = profileform.save(commit=False)
+            myprofile.user = request.user
+            myprofile.save()
+            return redirect(reverse('accounts:profile'))
+
+    else:
+        userform = UserForm(instance=request.user)
+        profileform = ProfileForm(instance=profile)
+
+    return render(request,'accounts/profile_edit.html',{'userform':userform , 'profileform':profileform})
